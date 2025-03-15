2f9dc2b758e1015fd4247a24d677d80f93ef2d30
sarathkumar1304 <raosarathkumar@gmail.com>
2025-03-15 13:19:45 +0530

    adding models

--- steps/model_building_step.py
+++ steps/model_building_step.py
@@ -1,221 +1,155 @@
-# from src.model_building import ModelBuilding
-# import os
-# import logging
-# import mlflow
-# import joblib
-# import pandas as pd
-# from sklearn.pipeline import Pipeline
 
-# from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
-# from zenml.client import Client
-# from zenml import ArtifactConfig, step, save_artifact
-# from zenml.enums import ArtifactType
-# from typing import Annotated
-# import numpy as np
-
-# # Get the active experiment tracker from ZenML
-# experiment_tracker = Client().active_stack.experiment_tracker
-
-# @step(enable_cache=False, experiment_tracker=experiment_tracker.name)
-# def model_building_step(
-#     model_name: str, 
-#     X_train: np.ndarray, 
-#     y_train: pd.Series
-# ) -> Annotated[Pipeline, ArtifactConfig(artifact_type=ArtifactType.MODEL)]:
-#     """
-#     ZenML step to build and train a model, log to MLflow, and save it as an artifact.
-
-#     :param model_name: Name of the model
-#     :param X_train: Training features
-#     :param y_train: Training labels
-#     :return: Trained pipeline model
-#     """
-#     logging.info(f"Model Building Step Initiated for: {model_name}")
-    
-#     try:
-#         # Ensure an MLflow run is active
-#         if not mlflow.active_run():
-#             mlflow.start_run(run_name=model_name, nested=True,log_system_metrics=True)
-        
-#         # Enable MLflow autologging
-#         mlflow.sklearn.autolog()
-
-#         #  Initialize ModelBuilding
-#         model_builder = ModelBuilding()
-#         model = model_builder.get_model(model_name, X_train, y_train)
-#         logging.info(f"Model '{model_name}' has been successfully created.")
-
-#         # Create the pipeline and train
-#         pipeline = Pipeline(steps=[("model", model)])
-#         y_train = np.array(y_train).ravel() 
-#         pipeline.fit(X_train, y_train)
-#         logging.info("Model training completed successfully.")
-
-#         # Save model using MLflow
-#         mlflow.sklearn.log_model(pipeline, artifact_path="model_pipeline")
-
-#         # Save model as ZenML artifact
-#         save_artifact(pipeline, name="trained_model", artifact_type=ArtifactType.MODEL)
-
-#         # Save model locally for backup
-#         model_dir = "models"
-#         os.makedirs(model_dir, exist_ok=True)
-#         model_path = os.path.join(model_dir, "model.pkl")
-#         joblib.dump(pipeline, model_path)
-#         logging.info(f"Model saved locally at {model_path}")
-
-#     except Exception as e:
-#         logging.error(f"An error occurred: {e}")
-#         raise e
-
-#     finally:
-#         mlflow.end_run()
-
-#     return pipeline
 
 # # # zenml experiment-tracker register mlflow_experiment_tracker --flavor=mlflow
 # # # zenml stack register custom_stack -e mlflow_experiment_tracker ... --set
 
-from src.model_building import ModelBuilding
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 import logging
 import mlflow
 import joblib
-import pandas as pd
-import numpy as np
+import optuna
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-
-from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+import xgboost as xgb
 from zenml.client import Client
 from zenml import ArtifactConfig, step, save_artifact
 from zenml.enums import ArtifactType
 from typing import Annotated
 
-# Get the active experiment tracker from ZenML
 experiment_tracker = Client().active_stack.experiment_tracker
 
 @step(enable_cache=False, experiment_tracker=experiment_tracker.name)
 def model_building_step(
     model_name: str, 
-    X_train: np.ndarray, 
-    y_train: pd.Series,
-    tune_hyperparameters: bool = False  # Enable or disable hyperparameter tuning
+    X_train: np.ndarray|pd.DataFrame, 
+    y_train: np.ndarray|pd.Series,
+    X_test :np.ndarray|pd.DataFrame,
+    y_test:np.ndarray|pd.Series,
+    tune_hyperparameters: bool = True,
+    n_trials:int=30
 ) -> Annotated[Pipeline, ArtifactConfig(artifact_type=ArtifactType.MODEL)]:
     """
-    ZenML step to build and train a model, perform optional hyperparameter tuning, 
-    log to MLflow, and save it as an artifact.
-
-    :param model_name: Name of the model
-    :param X_train: Training features
-    :param y_train: Training labels
-    :param tune_hyperparameters: Boolean flag for enabling hyperparameter tuning
-    :return: Trained pipeline model
+    ZenML step to build and train a model with Optuna hyperparameter tuning.
     """
     logging.info(f"Model Building Step Initiated for: {model_name}")
+    X_train = pd.DataFrame(X_train)
+    X_test = pd.DataFrame(X_test)
 
-    try:
-        # Ensure an MLflow run is active
-        if not mlflow.active_run():
-            mlflow.start_run(run_name=model_name, nested=True, log_system_metrics=True)
+    model_mapping = {
+        "linear_regression": LinearRegression,
+        "decision_tree": DecisionTreeRegressor,
+        "random_forest": RandomForestRegressor,
+        "adaboost": AdaBoostRegressor,
+        "gradient_boosting": GradientBoostingRegressor,
+        "xgboost": xgb.XGBRegressor,
+        "svm": SVR
+    }
+    # mlflow.set_experiment(model_name)
+    if not mlflow.active_run():
 
-        # Enable MLflow autologging
-        mlflow.sklearn.autolog()
-
-        # Initialize ModelBuilding
-        model_builder = ModelBuilding()
-        model = model_builder.get_model(model_name, X_train, y_train)
-
-        if tune_hyperparameters:
-            logging.info(f"Performing hyperparameter tuning for {model_name}.")
-
-            # Define hyperparameter grids for different models
-            param_grids = {
-
-                "linear_regression": {},  # No hyperparameters for basic Linear Regression
-
-                "decision_tree": {
-                    "criterion": ["squared_error", "friedman_mse", "absolute_error"],
-                    "max_depth": [5, 10, 20, None],
-                    "min_samples_split": [2, 5, 10],
-                    "min_samples_leaf": [1, 2, 4],
-                    "max_features": ["auto", "sqrt", "log2"],
-                },
-
-                "random_forest": {
-                    "n_estimators": [50, 100, 200, 500],
-                    "criterion": ["squared_error", "friedman_mse"],
-                    "max_depth": [5, 10, 20, None],
-                    "min_samples_split": [2, 5, 10],
-                    "min_samples_leaf": [1, 2, 4],
-                    "max_features": ["auto", "sqrt", "log2"],
-                    "bootstrap": [True, False],
-                },
-
-                "adaboost": {
-                    "n_estimators": [50, 100, 200, 500],
-                    "learning_rate": [0.001, 0.01, 0.1, 1],
-                    "loss": ["linear", "square", "exponential"],  # For regression tasks
-                },
-
-                "gradient_boosting": {
-                    "n_estimators": [50, 100, 200, 500],
-                    "learning_rate": [0.001, 0.01, 0.1, 0.2, 0.5],
-                    "max_depth": [3, 5, 10, None],
-                    "min_samples_split": [2, 5, 10],
-                    "min_samples_leaf": [1, 2, 4],
-                    "max_features": ["auto", "sqrt", "log2"],
-                    "subsample": [0.8, 0.9, 1.0],  # Helps prevent overfitting
-                },
-
-                "xgboost": {  # If you decide to use XGBoost
-                    "n_estimators": [50, 100, 200, 500],
-                    "learning_rate": [0.001, 0.01, 0.1, 0.2, 0.5],
-                    "max_depth": [3, 5, 10],
-                    "subsample": [0.7, 0.8, 0.9, 1.0],
-                    "colsample_bytree": [0.7, 0.8, 0.9, 1.0],
-                    "gamma": [0, 0.1, 0.2, 0.3],  # Regularization
-                    "lambda": [0, 0.01, 0.1, 1],  # L2 Regularization
-                }
+        mlflow.start_run(run_name=model_name ,log_system_metrics=True)
+    
+    # Enable MLflow autologging
+    mlflow.sklearn.autolog()
+    
+    def objective(trial):
+    # Define the parameter grid ONLY for the selected model
+        if model_name == "decision_tree":
+            param_grid = {
+                'max_depth': trial.suggest_int('max_depth', 3, 50),
+                'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+                'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 10),
             }
+        elif model_name == "linear_regression":
+            param_grid = {}
+        elif model_name == "random_forest":
+            param_grid = {
+                'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+                'max_depth': trial.suggest_int('max_depth', 3, 50),
+                'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+                'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 8),
+                'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
+                'bootstrap': trial.suggest_categorical('bootstrap', [True, False])
+            }
+        elif model_name == "adaboost":
+            param_grid = {
+                'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+                'learning_rate': trial.suggest_float('learning_rate', 0.01, 1.0),
+                'estimator': DecisionTreeRegressor()
+            }
+        elif model_name == "gradient_boosting":
+            param_grid = {
+                'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+                'max_depth': trial.suggest_int('max_depth', 3, 50),
+                'min_samples_split': trial.suggest_int('min_samples_split', 2, 20),
+                'min_samples_leaf': trial.suggest_int('min_samples_leaf', 1, 10),
+                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3),
+                'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+                'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2']),
+                'loss': trial.suggest_categorical('loss', ['squared_error', 'absolute_error'])
+            }
+        elif model_name == "xgboost":
+            param_grid =  {
+                'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+                'max_depth': trial.suggest_int('max_depth', 3, 50),
+                'learning_rate': trial.suggest_float('learning_rate', 0.01, 0.3),
+                'subsample': trial.suggest_float('subsample', 0.5, 1.0),
+                'colsample_bytree': trial.suggest_float('colsample_bytree', 0.5, 1.0),  # Added correct param
+                'gamma': trial.suggest_float('gamma', 0.0, 5.0),  # Regularization
+                'lambda': trial.suggest_float('lambda', 0.0, 10.0)  # L2 Regularization
+            }
+        elif model_name == "svm":
+            param_grid = {
+                'C': trial.suggest_float('C', 0.1, 100.0),
+                'epsilon': trial.suggest_float('epsilon', 0.01, 1.0),
+                'kernel': trial.suggest_categorical('kernel', ['linear', 'poly', 'rbf', 'sigmoid']),
+                'degree': trial.suggest_int('degree', 2, 5),
+                'gamma': trial.suggest_categorical('gamma', ['scale', 'auto']),
+            }
+        else:
+            raise ValueError(f"Model {model_name} is not supported.")
 
+        # Train model with selected parameters
+        model = model_mapping[model_name](**param_grid)
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        
+        # Return R² score for Optuna to optimize
+        return r2_score(y_test, y_pred)
 
-            param_grid = param_grids.get(model_name, {})
+    if tune_hyperparameters:
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective, n_trials=n_trials)
+        best_params = study.best_params
+        logging.info(f"Best parameters for {model_name}: {best_params}")
+    else:
+        best_params = {}
 
-            if param_grid:
-                grid_search = GridSearchCV(model, param_grid, cv=5, scoring="r2", n_jobs=-1)
-                grid_search.fit(X_train, y_train)
-                model = grid_search.best_estimator_
-                logging.info(f"Best Parameters for {model_name}: {grid_search.best_params_}")
-            else:
-                logging.info(f"No hyperparameter tuning performed for {model_name} (no hyperparameters available).")
+    # valid_params = {k: v for k, v in best_params.items() if v is not None}
 
-        logging.info(f"Model '{model_name}' has been successfully created.")
+#  Initialize the final model with only valid parameters
+    final_model = model_mapping[model_name](**best_params)
+    # final_model = model_mapping[model_name](**best_params)
+    final_model.fit(X_train, y_train)
 
-        # Create the pipeline and train
-        pipeline = Pipeline(steps=[("model", model)])
-        y_train = np.array(y_train).ravel()
-        pipeline.fit(X_train, y_train)
-        logging.info("Model training completed successfully.")
+    pipeline = Pipeline(steps=[("model", final_model)])
+    pipeline.fit(X_train, y_train)
 
-        # Save model using MLflow
-        mlflow.sklearn.log_model(pipeline, artifact_path="model_pipeline")
-
-        # Save model as ZenML artifact
-        save_artifact(pipeline, name="trained_model", artifact_type=ArtifactType.MODEL)
-
-        # Save model locally for backup
-        model_dir = "models"
-        os.makedirs(model_dir, exist_ok=True)
-        model_path = os.path.join(model_dir, model_name+"_model.pkl")
-        joblib.dump(pipeline, model_path)
-        logging.info(f"Model saved locally at {model_path}")
-
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        raise e
-
-    finally:
-        mlflow.end_run()
+    mlflow.sklearn.log_model(pipeline, artifact_path="model_pipeline")
+    save_artifact(pipeline, name="trained_model", artifact_type=ArtifactType.MODEL)
+    
+    model_dir = "models"
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.pkl")
+    joblib.dump(pipeline, model_path)
+    logging.info(f"Model saved locally at {model_path}")
 
     return pipeline
